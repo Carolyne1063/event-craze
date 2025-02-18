46579b5ccbe0ff58c993f0b6b8b1e93d094bd9b1
Carolyne1063 <carolmartoz763@gmail.com>
2025-02-17 17:27:01 -0800

    added admin children routing

--- frontend/eventcraze/src/app/app.routes.ts
+++ frontend/eventcraze/src/app/app.routes.ts
@@ -3,11 +3,25 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { DashboardComponent } from './components/admin-dashboard/dashboard/dashboard.component';
+import { EventsComponent } from './components/admin-dashboard/events/events.component';
+import { UsersComponent } from './components/admin-dashboard/users/users.component';
+import { BookingsComponent } from './components/admin-dashboard/bookings/bookings.component';
+import { SettingsComponent } from './components/admin-dashboard/settings/settings.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginRegisterComponent},
-    { path: 'admin', component: AdminDashboardComponent},
+    { path: 'admin', component: AdminDashboardComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'events', component: EventsComponent },
+            { path: 'users', component: UsersComponent },
+            { path: 'bookings', component: BookingsComponent },
+            { path: 'settings', component: SettingsComponent },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' } 
+        ]
+    },
     { path: 'user', component: UserDashboardComponent},
     { path: '**', redirectTo: 'home' }
 
--- frontend/eventcraze/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ frontend/eventcraze/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarComponent, RouterModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
