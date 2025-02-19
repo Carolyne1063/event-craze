96efc2e89d6b81f59309b9da2c98573219893553
Carolyne1063 <carolmartoz763@gmail.com>
2025-02-18 21:08:47 -0800

    added users page in admin dashboard

--- frontend/eventcraze/src/app/components/admin-dashboard/users/users.component.ts
+++ frontend/eventcraze/src/app/components/admin-dashboard/users/users.component.ts
@@ -1,12 +1,72 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+interface User {
+  id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  address: string;
+  imageUrl?: string;
+}
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  users: User[] = [
+    {
+      id: '1',
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@example.com',
+      phone: '+123456789',
+      address: '123 Main St, Cityville',
+      imageUrl: 'https://randomuser.me/api/portraits/men/1.jpg'
+    },
+    {
+      id: '2',
+      firstname: 'Jane',
+      lastname: 'Smith',
+      email: 'jane.smith@example.com',
+      phone: '+987654321',
+      address: '456 Elm St, Townsville',
+      imageUrl: 'https://randomuser.me/api/portraits/women/2.jpg'
+    },
+    {
+      id: '3',
+      firstname: 'Alice',
+      lastname: 'Johnson',
+      email: 'alice.johnson@example.com',
+      phone: '+192837465',
+      address: '789 Oak St, Villagetown',
+      imageUrl: 'https://randomuser.me/api/portraits/women/3.jpg'
+    },
+    {
+      id: '4',
+      firstname: 'Bob',
+      lastname: 'Williams',
+      email: 'bob.williams@example.com',
+      phone: '+5647382910',
+      address: '159 Pine St, Suburbia',
+      imageUrl: 'https://randomuser.me/api/portraits/men/4.jpg'
+    }
+  ];
 
+  selectedUser: User = this.users[0]; // Default selection
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectUser(user: User) {
+    this.selectedUser = user; // Update user details dynamically
+  }
 }
